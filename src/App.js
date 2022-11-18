cb96ad25b00763bd4feff8df53ab4fe172036480
Mikko Tanhola <mikko.tanhola@metropolia.fi>
2022-11-18 14:41:38 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import { useState } from 'react'
 
+const PersonList = (props) => {
+  const {list} = props
+  return (
+    <>
+      <ul style={{listStyle: "none"}}>
+        {list.map((person, i) => {return (
+          <li key={i}>{person.name} {person.number}</li>
+        )})}
+      </ul>
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName,
+      number: newNum
+    }
+
+    const filtered = persons.filter(person => person.name === newName).pop()
+
+    if(filtered){
+      alert(`${newName} is already added to phonebook`)
+    } 
+    else{
+      setPersons(persons.concat(person))
+    }
+    setNewName('')
+  }
+
+
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={({target}) => setNewName(target.value)}/>
+        </div>
+        <div>
+          number: <input value={newNum} onChange={({target}) => setNewNum(target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <PersonList list={persons}/>
+      </div>
     </div>
   )
 }
